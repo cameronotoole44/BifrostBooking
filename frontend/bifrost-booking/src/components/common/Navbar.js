ee0fa0829142bf9d106c4f9fd2feba2b903a144a
cameronotoole44 <cameronotoole404@gmail.com>
2024-07-14 23:16:38 -0400

    styling tweaks

--- frontend/bifrost-booking/src/components/common/Navbar.js
+++ frontend/bifrost-booking/src/components/common/Navbar.js
@@ -3,19 +3,19 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav className="bg-nightshade-950 p-4 shadow-lg">
+        <nav className="bg-sand-100 p-4 shadow-lg">
             <div className="container mx-auto flex items-center justify-between">
-                <Link to="/" className="text-thunder-200 text-xl font-bold">
+                <Link to="/" className="text-sky-900 text-xl font-bold hover:text-sky-300">
                     Bifröst Bookings
                 </Link>
                 <div className="space-x-4">
-                    <Link to="/search" className="text-thunder-200 hover:text-sky-300">
+                    <Link to="/search" className="text-sky-900 font-bold hover:text-sky-300">
                         Search Flights
                     </Link>
-                    <Link to="/register" className="text-thunder-200 hover:text-sky-300">
+                    <Link to="/register" className="text-sky-900 font-bold hover:text-sky-300">
                         Register
                     </Link>
-                    <Link to="/login" className="text-thunder-200 hover:text-sky-300">
+                    <Link to="/login" className="text-sky-900 font-bold hover:text-sky-300">
                         Login
                     </Link>
                 </div>
