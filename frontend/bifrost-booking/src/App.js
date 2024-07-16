a16e1b2313562e9b65e995e5d5b3237930ae2cc6
cameronotoole44 <cameronotoole404@gmail.com>
2024-07-15 21:14:38 -0400

    changed paths

--- frontend/bifrost-booking/src/App.js
+++ frontend/bifrost-booking/src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
+import Home from './components/flight/Home';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Profile from './components/auth/Profile';
 import AdminDashboard from './components/admin/AdminDashboard';
 import ManageBookings from './components/admin/ManageBookings';
 import ManageFlights from './components/admin/ManageFlights';
-import BookingList from './components/booking/BookingList';
 import FlightDetails from './components/flight/FlightDetails';
-import Search from './components/Search';
+import Search from './components/flight/Search';
 import Navbar from './components/common/Navbar';
 import Footer from './components/common/Footer';
 import './App.css';
@@ -26,7 +25,6 @@ const App = () => {
         <Route path="/admin/dashboard" element={<AdminDashboard />} />
         <Route path="/admin/manage-bookings" element={<ManageBookings />} />
         <Route path="/admin/manage-flights" element={<ManageFlights />} />
-        <Route path="/bookings" element={<BookingList />} />
         <Route path="/flight-details" element={<FlightDetails />} />
         <Route path="/search" element={<Search />} />
       </Routes>
