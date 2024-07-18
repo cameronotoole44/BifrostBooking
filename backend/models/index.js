1f0a135898646a0ba01ce0dc88e047a402c6ce0c
cameronotoole44 <cameronotoole404@gmail.com>
2024-07-18 16:00:10 -0400

    restructured

--- backend/models/index.js
+++ backend/models/index.js
@@ -6,14 +6,9 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-// Import models directly
-db.Flight = require('./flight'); // No need to pass sequelize and DataTypes
+// MODEL IMPORT //
+db.Flight = require('./flight');
 db.User = require('./user');
 db.Booking = require('./booking');
-
-// Define associations if needed
-// Example:
-// db.Flight.belongsTo(db.User);
-// db.User.hasMany(db.Flight);
 
 module.exports = db;
