665e152c22c3217f616fd3390097993c7b616d23
cameronotoole44 <cameronotoole404@gmail.com>
2024-07-25 11:15:56 -0400

    +

--- frontend/bifrost-booking/src/reducers/index.js
+++ frontend/bifrost-booking/src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import bookingReducer from './bookingReducer';
 import flightReducer from './flightReducer';
 import userReducer from './userReducer';
+import adminReducer from './adminReducer';
 
 const rootReducer = combineReducers({
     bookings: bookingReducer,
     flights: flightReducer,
-    user: userReducer
+    user: userReducer,
+    admin: adminReducer,
 });
 
 export default rootReducer;
