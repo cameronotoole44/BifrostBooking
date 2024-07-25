5bee9650eb0d9632357f81eed021aa089fb5ede3
cameronotoole44 <cameronotoole404@gmail.com>
2024-07-25 10:36:11 -0400

    custom error page

---
+++ frontend/bifrost-booking/src/components/pages/Error404.js
@@ -0,0 +1,13 @@
+function error404() {
+    return (
+
+        <main>
+            <h1>404: PAGE NOT FOUND</h1>
+            <p>this page is taking a break🍙</p>
+            <img src="../../assets/images/error404.jpg" alt="odin napping" />
+        </main>
+
+    )
+};
+
+export default error404;
