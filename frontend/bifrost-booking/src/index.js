ebfba2caed167adf3b2f4037efc54bfddccc115b
cameronotoole44 <cameronotoole404@gmail.com>
2024-07-25 11:31:44 -0400

    provider

--- frontend/bifrost-booking/src/index.js
+++ frontend/bifrost-booking/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import App from './App';
+import CurrentUserProvider from './contexts/CurrentUser';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(
+ReactDOM.render(
     <Provider store={store}>
-        <App />
-    </Provider>
+        <CurrentUserProvider>
+            <App />
+        </CurrentUserProvider>
+    </Provider>,
+    document.getElementById('root')
 );
