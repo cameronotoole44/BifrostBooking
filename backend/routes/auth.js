2e11cdf8ea59e9f7cd03d1d3f31cddedd7f17a0c
cameronotoole44 <cameronotoole404@gmail.com>
2024-07-27 23:02:47 -0400

    login works FINALLY

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { registerUser, loginUser } = require('../controllers/authentication');
 
 router.post('/register', registerUser);
-// router.post('/login', loginUser);
+router.post('/login', loginUser);
 
 module.exports = router;
