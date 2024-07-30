680eebdbd553f4dfe4120b9604e9a1b04cbf80b4
cameronotoole44 <cameronotoole404@gmail.com>
2024-07-29 21:54:21 -0400

    refactored old reducers out

--- frontend/bifrost-booking/src/reducers/index.js
+++ frontend/bifrost-booking/src/reducers/index.js
@@ -2,15 +2,12 @@ import { combineReducers } from 'redux';
 import bookingReducer from './bookingReducer';
 import flightReducer from './flightReducer';
 import userReducer from './userReducer';
-import userLoginReducer from './loginReducer';
 import adminReducer from './adminReducer';
-
 
 const rootReducer = combineReducers({
     bookings: bookingReducer,
     flights: flightReducer,
     user: userReducer,
-    userLogin: userLoginReducer,
     admin: adminReducer,
 });
 
