619665c41b4b178f0fc96a908468d655514a495f
cameronotoole44 <cameronotoole404@gmail.com>
2024-08-03 23:48:08 -0400

    navbar styling, updated login, added img

--- frontend/bifrost-booking/src/components/auth/Login.js
+++ frontend/bifrost-booking/src/components/auth/Login.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { loginUser } from '../../actions/userActions';
+import aurora from '../../assets/images/aurora.jpg';
+import loginIcon from '../../assets/images/login_icon.png';
 
 const Login = () => {
     const [email, setEmail] = useState('');
@@ -23,38 +25,40 @@ const Login = () => {
     }, [currentUser, navigate]);
 
     return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-100">
-            <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-                <h2 className="text-2xl font-bold mb-6 text-center text-gray-900">Login</h2>
-                <form onSubmit={handleSubmit} className="space-y-4">
-                    <div>
-                        <input
-                            type="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            placeholder="Email"
-                            required
-                            className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        />
-                    </div>
-                    <div>
-                        <input
-                            type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            placeholder="Password"
-                            required
-                            className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        />
-                    </div>
-                    <button
-                        type="submit"
-                        className="w-full bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    >
-                        {loading ? 'Loading...' : 'Login'}
-                    </button>
-                </form>
-                {error && <p className="text-red-500 mt-4">{error}</p>}
+        <div style={{ backgroundImage: `url(${aurora})`, backgroundSize: 'cover', backgroundPosition: 'center', minHeight: '100vh' }}>
+            <div className="flex items-center justify-center min-h-screen">
+                <div className="bg-smoke-200 p-8 rounded-lg shadow-lg w-full max-w-md">
+                    <h2 className="text-2xl font-bold mb-6 text-center text-sky-400"> <img src={loginIcon} alt="Login Icon" className="w-24 h-24 mr-4" /> </h2>
+                    <form onSubmit={handleSubmit} className="space-y-4">
+                        <div>
+                            <input
+                                type="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                placeholder="Email"
+                                required
+                                className="w-full p-3 border border-moss-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-moss-300"
+                            />
+                        </div>
+                        <div>
+                            <input
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                placeholder="Password"
+                                required
+                                className="w-full p-3 border border-moss-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-moss-300"
+                            />
+                        </div>
+                        <button
+                            type="submit"
+                            className="w-full bg-smoke-100 text-coal p-3 rounded-lg hover:bg-moss-300 focus:outline-none focus:ring-2 focus:ring-sunrise-800"
+                        >
+                            {loading ? 'Loading...' : 'Login'}
+                        </button>
+                    </form>
+                    {error && <p className="text-salmon-800 mt-4">{error}</p>}
+                </div>
             </div>
         </div>
     );
--- frontend/bifrost-booking/src/components/pages/Navbar.js
+++ frontend/bifrost-booking/src/components/pages/Navbar.js
@@ -1,89 +1,163 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { logoutUser } from '../../actions/userActions';
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
-import logo from '../../assets/images/logo.png';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { logoutUser } from "../../actions/userActions";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import logo from "../../assets/images/logo.png";
 
 const Navbar = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const currentUser = useSelector((state) => state.user.currentUser);
     const [isOpen, setIsOpen] = useState(false);
+    const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
 
     const handleLogout = () => {
         dispatch(logoutUser());
-        navigate('/');
+        navigate("/");
     };
 
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const toggleProfileMenu = () => {
+        setIsProfileMenuOpen(!isProfileMenuOpen);
+    };
+
+    const closeMenu = () => {
+        setIsOpen(false);
+        setIsProfileMenuOpen(false);
+    };
+
+    // Links for unauthenticated users
     let loginActions = (
         <div className="flex space-x-4">
             <Link
                 to="/auth/register"
-                className="text-sky-900 font-bold hover:text-sky-300 py-2 px-4 rounded border border-sky-900 hover:bg-sky-100"
+                className="block text-sky-900 font-medium hover:text-sky-300 py-2 px-4 rounded-md hover:bg-smoke-500"
+                onClick={closeMenu}
             >
                 Register
             </Link>
             <Link
                 to="/auth/login"
-                className="text-sky-900 font-bold hover:text-sky-300 py-2 px-4 rounded border border-sky-900 hover:bg-sky-100"
+                className="block text-sky-900 font-medium hover:text-sky-300 py-2 px-4 rounded-md hover:bg-smoke-500"
+                onClick={closeMenu}
             >
                 Login
             </Link>
         </div>
     );
 
+    // Links for authenticated users
     if (currentUser) {
         loginActions = (
-            <div className="flex items-center space-x-4">
-                <button
-                    onClick={handleLogout}
-                    className="text-sky-900 font-bold hover:text-sky-300 py-2 px-4 rounded border border-sky-900 hover:bg-sky-100"
+            <>
+                {/* Displaying Settings and Dashboard Links on Mobile and Desktop */}
+                <Link
+                    to="/settings"
+                    className="block text-sky-900 font-medium hover:text-sky-300 py-2 px-4 rounded-md hover:bg-smoke-500"
+                    onClick={closeMenu}
                 >
-                    Logout
-                </button>
-                <span className="text-sky-900 font-bold">
-                    Logged in as {currentUser.firstName} {currentUser.lastName}
-                </span>
-            </div>
+                    Settings
+                </Link>
+                <Link
+                    to="/dashboard"
+                    className="block text-sky-900 font-medium hover:text-sky-300 py-2 px-4 rounded-md hover:bg-smoke-500"
+                    onClick={closeMenu}
+                >
+                    Dashboard
+                </Link>
+            </>
         );
     }
 
     return (
-        <nav className="bg-sand-100 p-4 shadow-lg">
+        <nav className="bg-smoke-300 p-4 shadow-lg relative z-50">
             <div className="container mx-auto flex items-center justify-between">
                 <div className="flex items-center">
-                    <Link to="/">
+                    <Link to="/" onClick={closeMenu}>
                         <img src={logo} alt="Bifröst Bookings Logo" className="h-10 w-auto mr-4" />
                     </Link>
-                    <Link to="/" className="text-sky-900 text-xl font-bold hover:text-sky-300">
+                    <Link
+                        to="/"
+                        className="text-sky-900 text-xl font-bold hover:text-sky-300"
+                        onClick={closeMenu}
+                    >
                         Bifröst Bookings
                     </Link>
                 </div>
+                {/* Mobile menu toggle */}
                 <div className="md:hidden">
-                    <button onClick={() => setIsOpen(!isOpen)} className="text-sky-900">
+                    <button onClick={toggleMenu} className="text-sky-900">
                         {isOpen ? <XMarkIcon className="h-6 w-6" /> : <Bars3Icon className="h-6 w-6" />}
                     </button>
                 </div>
-                <div className={`md:flex space-x-4 ${isOpen ? 'block' : 'hidden'}`}>
-                    <Link to="/search" className="text-sky-900 font-bold hover:text-sky-300 py-2 px-4 rounded border border-sky-900 hover:bg-sky-100">
-                        Search Flights
-                    </Link>
-                    {currentUser && (
-                        <>
-                            <Link to="/settings" className="text-sky-900 font-bold hover:text-sky-300 py-2 px-4 rounded border border-sky-900 hover:bg-sky-100">
-                                Settings
-                            </Link>
-                            <Link to="/dashboard" className="text-sky-900 font-bold hover:text-sky-300 py-2 px-4 rounded border border-sky-900 hover:bg-sky-100">
-                                Dashboard
-                            </Link>
-                        </>
-                    )}
-                    {loginActions}
+                {/* Menu Links */}
+                <div
+                    className={`${isOpen
+                        ? "absolute top-full right-0 left-0 bg-smoke-400 shadow-lg mt-2 rounded-md w-full md:w-auto"
+                        : "hidden md:flex"
+                        } md:flex items-center space-x-4 p-4 md:p-0 overflow-y-auto max-h-60 md:max-h-auto`}
+                >
+                    <div className="flex flex-col md:flex-row md:space-x-4">
+                        <Link
+                            to="/search"
+                            className="block text-sky-900 font-medium hover:text-sky-300 py-2 px-4 rounded-md hover:bg-smoke-500"
+                            onClick={closeMenu}
+                        >
+                            Search Flights
+                        </Link>
+                        {loginActions}
+                        {currentUser && (
+                            <button
+                                onClick={() => {
+                                    handleLogout();
+                                    closeMenu();
+                                }}
+                                className="block text-sky-900 font-medium hover:text-sky-300 py-2 px-4 rounded-md hover:bg-smoke-500 text-left"
+                            >
+                                Logout
+                            </button>
+                        )}
+                    </div>
                 </div>
+                {/* Profile Button for Desktop */}
+                {currentUser && (
+                    <div className="relative hidden md:block">
+                        <button
+                            onClick={toggleProfileMenu}
+                            className="flex items-center justify-center h-8 w-8 bg-sky-400 text-sky-950 font-bold rounded-full"
+                        >
+                            {currentUser.firstName[0]}
+                            {currentUser.lastName[0]}
+                        </button>
+                        {isProfileMenuOpen && (
+                            <div className="absolute right-0 mt-2 w-48 bg-smoke-950 text-smoke-50 rounded-md shadow-lg z-10">
+                                <Link
+                                    to="/settings"
+                                    className="block px-4 py-2 hover:text-sky-200 hover:bg-smoke-900"
+                                    onClick={closeMenu}
+                                >
+                                    Settings
+                                </Link>
+                                <button
+                                    onClick={() => {
+                                        handleLogout();
+                                        closeMenu();
+                                    }}
+                                    className="block px-4 py-2 w-full text-left hover:bg-smoke-900"
+                                >
+                                    Logout
+                                </button>
+                            </div>
+                        )}
+                    </div>
+                )}
             </div>
         </nav>
-    )
+    );
 };
 
 export default Navbar;
